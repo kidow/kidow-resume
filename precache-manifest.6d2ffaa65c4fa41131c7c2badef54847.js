92ad7a4952427f8ffcd9d9030a35fc3fdba216d8
kidow <39021158+kidow@users.noreply.github.com>
2018-12-13 20:53:18 +0900

    initial commit

---
+++ precache-manifest.6d2ffaa65c4fa41131c7c2badef54847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3543e9c99b73fd0f0909e4cb32d4ac62",
+    "url": "/static/media/Image.3543e9c9.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80c4877e619f20a647cf",
+    "url": "/static/js/main.80c4877e.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "80c4877e619f20a647cf",
+    "url": "/static/css/main.ce810ab3.chunk.css"
+  },
+  {
+    "revision": "2177ccbd69ffc76528d29f9557a374eb",
+    "url": "/index.html"
+  }
+];
